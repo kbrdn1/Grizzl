5926949457603caa2a82ba0a95f3ec9cbc6cb2d8
Kylian Bardini <kylianb1@icloud.com>
2023-07-04 10:01:06 +0200

    ♻️ refactor: prettier format

--- api/src/db/connect.js
+++ api/src/db/connect.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-const { MONGODB_URI, MONGODB_PASS, MONGODB_USER } = process.env;
+const { MONGODB_URI, MONGODB_PASS, MONGODB_USER } = process.env
 const connect = async () => {
-    try {
-        await mongoose.connect(MONGODB_URI, {
-            pass: MONGODB_PASS,
-            user: MONGODB_USER,
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('Connected to MongoDB');
-    } catch (err) {
-        console.log('Error connecting to MongoDB', err);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      pass: MONGODB_PASS,
+      user: MONGODB_USER,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    console.log('Connected to MongoDB')
+  } catch (err) {
+    console.log('Error connecting to MongoDB', err)
+    process.exit(1)
+  }
 }
 
-module.exports = connect;
+module.exports = connect
--- api/src/index.js
+++ api/src/index.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const db = require('./db/connect');
-require('dotenv').config();
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const db = require('./db/connect')
+require('dotenv').config()
 
 // env variables
-const { PORT } = process.env;
+const { PORT } = process.env
 
 // app
-const app = express();
+const app = express()
 
 // db
-db();
+db()
 
 // cors
-app.use(cors());
+app.use(cors())
 
 // body parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // start server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`)
 })
