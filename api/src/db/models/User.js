6ce7f4f0ad341a81507381efec393ab01c490330
Kylian Bardini <kylianb1@icloud.com>
2023-07-04 09:57:50 +0200

    🗃️ feat: add userSchema

---
+++ api/src/db/models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true,
+        minLength: 3,
+        maxLength: 20,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+    }
+});
